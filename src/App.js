769b406c24c757158af6cbc299ce2d0491ca5420
AbdulKhalequegithub <abdul.khaleque@clouddrove.com>
2023-02-23 21:51:45 +0530

    version 72

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from pandaboi
+          Learn React latest v1
         </a>
       </header>
     </div>
